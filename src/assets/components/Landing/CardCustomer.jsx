72e2e149435af1adf8c625efe26fb0606fce5642
aqilknz <khairunnz123@gmail.com>
2026-04-06 09:44:55 +0700

    feat: add CardCustomer components

---
+++ src/assets/components/Landing/CardCustomer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function CardCustomer({path,name,rating, review}) {
+  return (
+    <div className='bg-white flex flex-col justify-center items-center'>
+        <div className='bg-secondary px-5 py-15 mx-10 w-fit flex flex-col justify-center items-center text-center my-5 gap-5 rounded-xl'>
+            <img src={path} alt={name}/>
+            <h3 className='font-bold text-2xl'>{name}</h3>
+            <img src={rating} className='w-1/2'/>
+            <p className='text-5xl font-extrabold'>“</p>
+            <p className='w-full text-center text-lg'>{review}</p>
+        </div>
+    </div>
+  )
+}
+
+export default CardCustomer
