6a64daf6f243acff962d0bb72b734b1359525f10
aqilknz <khairunnz123@gmail.com>
2026-04-06 10:19:13 +0700

    docs: add JSDoc for CardPartner component

--- src/assets/components/Landing/CardPartners.jsx
+++ src/assets/components/Landing/CardPartners.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+/**
+ * 
+ * @param {object} props 
+ * @param {string} props.partner
+ * @returns {JSX.Element}
+ */
+
 function CardPartners({partner}) {
   return (
     <div className='w-full h-full flex flex-col gap-10 justify-center items-center bg-red rounded-lg'>
