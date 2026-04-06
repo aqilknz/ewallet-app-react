59d165da1a2766ca8f960f4bcc55046271484bbf
aqilknz <khairunnz123@gmail.com>
2026-04-06 10:21:54 +0700

    docs: add JSDoc for Social Media component

--- src/assets/components/Landing/SocialMedia.jsx
+++ src/assets/components/Landing/SocialMedia.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+/**
+ * 
+ * @param {object} props 
+ * @param {string} props.sosmed
+ * @param {string} props.alt
+ * @returns {JSX.Element}
+ */
+
 function SocialMedia({ sosmed, alt }) {
     return (
         <button className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer'>
