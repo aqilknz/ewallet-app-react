d5a7ab8136ea9dd01d6861e29d372e1155832253
aqilknz <khairunnz123@gmail.com>
2026-04-06 09:40:18 +0700

    feat: add ButtonSubmit component

---
+++ src/assets/components/Auth/ButtonSubmit.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../../Global.css'
+
+function ButtonSubmit({ label }) {
+    return (
+        <div className='w-full'>
+            <button type="submit" className='flex flex-col justify-center w-full items-center bg-primary text-white font-monserrat font-bold py-2 px-4 my-2 rounded-lg cursor-pointer hover:bg-blue-800'>{label}</button>
+        </div>
+    )
+}
+
+export default ButtonSubmit
