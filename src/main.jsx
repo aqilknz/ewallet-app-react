d32dfaec4f571b5f683a8ad9661159ac51945188
aqilknz <khairunnz123@gmail.com>
2026-04-06 09:58:11 +0700

    feat: add main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+import { BrowserRouter } from 'react-router-dom'
+import AppRoute from './assets/router/AppRoute.jsx'
+import './Global.css'
+// import App from './App.jsx'
+import Login from './assets/pages/Login.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRoute />
+    </BrowserRouter>
   </StrictMode>,
 )
