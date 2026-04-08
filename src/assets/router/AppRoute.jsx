b11ece4807924d9bec7bd3eb284005573700ce61
aqilknz <khairunnz123@gmail.com>
2026-04-08 09:23:13 +0700

    feat: add nested routing

--- src/assets/router/AppRoute.jsx
+++ src/assets/router/AppRoute.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import Login from '../pages/Login.jsx'
 import Register from '../pages/Register.jsx'
 import LandingPage from '../pages/LandingPage.jsx'
-import Dashboard from '../pages/DashboardP.jsx'
+import Dashboard from '../pages/Dashboard.jsx'
 import Transfer from '../pages/Transfer.jsx'
+import History from '../pages/History.jsx'
+import TopUp from '../pages/topup.jsx'
 
 // import Register from '../pages/Register.jsx'
 // import Dashboard from '../pages/Dashboard.jsx'
@@ -12,11 +14,15 @@ import Transfer from '../pages/Transfer.jsx'
 function AppRoute() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path='/dashboard'element={<Dashboard />}/>
-      <Route path='/transfer'element={<Transfer />}/>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='auth'>
+        <Route index element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+      <Route path='dashboard' element={<Dashboard />} />
+      <Route path='transfer' element={<Transfer />} />
+      <Route path='history' element={<History />} />
+      <Route path='topup' element={<TopUp />} />
     </Routes>
   )
 }
