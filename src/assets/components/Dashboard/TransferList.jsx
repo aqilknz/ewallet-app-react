cad61cef502dc041ca255e3406fd4f099cfc6c41
aqilknz <khairunnz123@gmail.com>
2026-04-08 09:25:36 +0700

    feat: add transfer list

---
+++ src/assets/components/Dashboard/TransferList.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+function TransferList() {
+    const dataTransfer = [
+        { path: '/images/1.svg', name: 'Ghaluh1', telp: '082116304337', money: 'Rp.50.000', icon: '/icons/Star.svg' },
+        { path: '/images/1-2.svg', name: 'Cameron Williamson', telp: '082116312317', money: 'Rp.50.000', icon: '/icons/Star.svg' },
+        { path: '/images/1-3.svg', name: 'Cody Fisher', telp: '0812357163043', money: 'Rp.50.000', icon: '/icons/Star.svg' },
+        { path: '/images/1.svg', name: 'Ghaluh1', telp: '082116304337', money: 'Rp.50.000', icon: '/icons/Star.svg' },
+        { path: '/images/1-2.svg', name: 'Cameron Williamson', telp: '082116312317', money: 'Rp.50.000', icon: '/icons/Star.svg' },
+        { path: '/images/1.svg', name: 'Cody Fisher', telp: '0812357163043', money: 'Rp.50.000', icon: '/icons/Star.svg' },
+        { path: '/images/1-2.svg', name: 'Ghaluh1', telp: '082116304337', money: 'Rp.50.000', icon: '/icons/Star.svg' },
+        { path: '/images/1-3.svg', name: 'Cameron Williamson', telp: '082116312317', money: 'Rp.50.000', icon: '/icons/Star.svg' },
+        { path: '/images/1.svg', name: 'Cody Fisher', telp: '0812357163043', money: 'Rp.50.000', icon: '/icons/Star.svg' }
+    ]
+    return (
+        <div className="w-full overflow-x-auto">
+            <table className="w-full border-separate border-spacing-y-3">
+                <tbody>
+                    {dataTransfer.map((item, index) => {
+                        const isPos = item.pos;
+                        return (
+                            <tr
+                                key={index}
+                                className="bg-white hover:bg-secondary transition-colors odd:bg-gray-100"
+                            >
+                                <td className="p-2 w-16">
+                                    <img
+                                        src={item.path}
+                                        alt={`${item.name} icon`}
+                                        className="w-10 h-10"
+                                    />
+                                </td>
+
+                                <td className="p-2 ">
+                                    <div className="font-bold text-black text-center">
+                                        {item.name}
+                                    </div>
+                                </td>
+
+                                <td className="p-2 ">
+                                    <div className='text-center'>
+                                        {item.telp}
+                                    </div>
+                                </td>
+
+                                <td className="p-2 text-center w-16">
+                                    <img
+                                        src={item.icon}
+                                        alt="delete"
+                                        className="w-5 h-5 cursor-pointer "
+                                    />
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default TransferList
