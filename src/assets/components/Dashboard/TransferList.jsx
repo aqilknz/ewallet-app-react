950b4c3bb9ce305a545c5d39eec9a50044006e78
aqilknz <khairunnz123@gmail.com>
2026-04-15 08:24:12 +0700

    feat: add filter data components

--- src/assets/components/Dashboard/TransferList.jsx
+++ src/assets/components/Dashboard/TransferList.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router';
 
-function TransferList() {
+function TransferList({search}) {
+    const navigate = useNavigate();
     const dataTransfer = [
         { path: '/images/1.svg', name: 'Ghaluh1', telp: '082116304337', money: 'Rp.50.000', icon: '/icons/Star.svg' },
         { path: '/images/1-2.svg', name: 'Cameron Williamson', telp: '082116312317', money: 'Rp.50.000', icon: '/icons/Star.svg' },
@@ -12,16 +14,22 @@ function TransferList() {
         { path: '/images/1-3.svg', name: 'Cameron Williamson', telp: '082116312317', money: 'Rp.50.000', icon: '/icons/Star.svg' },
         { path: '/images/1.svg', name: 'Cody Fisher', telp: '0812357163043', money: 'Rp.50.000', icon: '/icons/Star.svg' }
     ]
+    const safeSearch = (search || '').toLowerCase();
+    const filteredData = dataTransfer.filter((item) =>
+        item.name.toLowerCase().includes(safeSearch.toLowerCase()) ||
+        item.telp.includes(safeSearch)
+    )
     return (
         <div className="w-full overflow-x-auto">
             <table className="w-full border-separate border-spacing-y-3">
                 <tbody>
-                    {dataTransfer.map((item, index) => {
+                    {filteredData.map((item,index) => {
                         const isPos = item.pos;
                         return (
                             <tr
                                 key={index}
-                                className="bg-white hover:bg-secondary transition-colors odd:bg-gray-100"
+                                className="bg-white hover:bg-secondary transition-colors odd:bg-gray-100 cursor-pointer group"
+                                onClick={() => navigate('/transfer/detail')}
                             >
                                 <td className="p-2 w-16">
                                     <img
@@ -36,6 +44,9 @@ function TransferList() {
                                         {item.name}
                                     </div>
                                 </td>
+                                {/* <NavLink to="/transfer/detail" className="p-2 text-center font-medium text-black">
+                                    {item.name}
+                                </NavLink> */}
 
                                 <td className="p-2 ">
                                     <div className='text-center'>
