2e6328cccb364f0fdd9ea28373782d4cebb7b972
aqilknz <khairunnz123@gmail.com>
2026-04-15 08:37:03 +0700

    feat: add styling button sign in component

--- src/assets/components/Auth/ButtonSignIn.jsx
+++ src/assets/components/Auth/ButtonSignIn.jsx
@@ -11,12 +11,15 @@ import React from 'react'
 
 function ButtonSignIn({ path, alt, text }) {
     return (
-        <div>
-            <button className='flex justify-center w-full items-center gap-2 bg-white text-black font-monserrat font-bold py-2 px-4 my-2 rounded-full border-secondary border cursor-pointer hover:bg-blue-500 hover:text-white [&:-webkit-autofill]:bg-white! [&:-webkit-autofill]:text-gray-900!'>
-                <img src={path} alt={alt} />
-                <span className='hidden md:block'>{text}</span>
-            </button>
-        </div>
+        <button
+            className="flex justify-center items-center gap-2 w-full 
+      bg-white text-black font-monserrat font-bold py-2 px-4 my-2 
+      rounded-full border-secondary border cursor-pointer 
+      hover:bg-blue-500 hover:text-white"
+        >
+            <img src={path} alt={alt} />
+            <span className="hidden md:block">{text}</span>
+        </button>
     )
 }
 
