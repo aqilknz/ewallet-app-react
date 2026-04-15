bf1d2b62aff8dedc969a09032e6e32c14a695cf9
aqilknz <khairunnz123@gmail.com>
2026-04-15 08:29:35 +0700

    feat: add Header component with Redux state logout and display current user email

--- src/assets/components/Dashboard/Header.jsx
+++ src/assets/components/Dashboard/Header.jsx
@@ -1,17 +1,23 @@
-import React, {  useState } from 'react';
-import {Link, useNavigate} from 'react-router'
+import React, { useState } from 'react';
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from 'react-router'
+import { logout } from '../../redux/slice/authSlice';
+import { useDispatch } from "react-redux";
+import { persistor } from '../../redux/store';
 
 function Header() {
+    const currentUser = useSelector((state)=>state.user.currentUser)
     const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate()
+    const dispatch = useDispatch()
     function handleLogout() {
-    localStorage.removeItem("currentUser");
-
-    navigate("/auth", { replace: true });
-}
+        dispatch(logout())
+        persistor.purge();
+        navigate("/auth", { replace: true });
+    }
 
     return (
-        <header className='bg-white min-w-screen border-b-2 border-secondary sticky top-0 z-50'>
+        <header className='bg-white min-w-full border-b-2 border-secondary sticky top-0 z-50'>
             <div className='flex w-full px-6 md:px-10 justify-between items-center h-20'>
 
                 <div className='flex justify-center items-center gap-2'>
@@ -24,11 +30,11 @@ function Header() {
                         className='flex items-center cursor-pointer gap-2 hover:bg-gray-50 p-2 rounded-xl transition-all'
                         onClick={() => setIsOpen(!isOpen)}
                     >
-                        <span>Ghaluh Wizard</span>
+                        <span>{currentUser?.email || "Guest"}</span>
                         <img src='/images/Ghaluh icon.svg' className='w-10 h-10 rounded-full object-cover' alt="User" />
                         <img
                             src='/icons/down.svg'
-                            className={`w-6 h-6 md:hidden transition-transform duration-300 ${isOpen ? 'rotate-180' : ''}`}
+                            className={`w-6 h-6 transition-transform duration-300 ${isOpen ? 'md:rotate-180' : ''}`}
                             alt="Arrow"
                         />
                     </div>
@@ -36,27 +42,27 @@ function Header() {
                     {isOpen && (
                         <div className="absolute right-0 top-full mt-2 w-56 bg-white border border-gray-100 rounded-2xl shadow-xl py-3 z-10 md:hidden">
                             <div className="px-2 space-y-1">
-                                <Link to="/ewallet/v1/dashboard"
+                                <Link to="/dashboard"
                                     className="block px-3 py-2 text-sm text-gray-700 hover:bg-primary hover:text-white rounded-lg transition">
                                     Dashboard
                                 </Link>
 
-                                <Link to="/ewallet/v1/transfer"
+                                <Link to="/transfer"
                                     className="block px-3 py-2 text-sm text-gray-700 hover:bg-primary hover:text-white rounded-lg transition">
                                     Transfer
                                 </Link>
 
-                                <Link to="/ewallet/v1/history"
+                                <Link to="/history"
                                     className="block px-3 py-2 text-sm text-gray-700 hover:bg-primary hover:text-white rounded-lg transition">
                                     History
                                 </Link>
 
-                                <Link to="/ewallet/v1/topup"
+                                <Link to="/topup"
                                     className="block px-3 py-2 text-sm text-gray-700 hover:bg-primary hover:text-white rounded-lg transition">
                                     Top Up
                                 </Link>
 
-                                <Link to="/ewallet/v1/profile"
+                                <Link to="/profile"
                                     className="block px-3 py-2 text-sm text-gray-700 hover:bg-primary hover:text-white rounded-lg transition">
                                     Profile
                                 </Link>
