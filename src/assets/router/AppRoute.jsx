3a261ee3f5c81a984626da740c48ec3141631e9d
aqilknz <khairunnz123@gmail.com>
2026-04-15 08:50:13 +0700

    feat: add Provider, PersistGate and Toaster

--- src/assets/router/AppRoute.jsx
+++ src/assets/router/AppRoute.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Routes, Route, Outlet } from 'react-router-dom'
+import { Provider } from "react-redux";
+import { store, persistor } from "../redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from 'react-hot-toast';
+import AuthProtect from '../components/Auth/AuthProtect.jsx';
 import Login from '../pages/Login.jsx'
 import Register from '../pages/Register.jsx'
 import LandingPage from '../pages/LandingPage.jsx'
@@ -7,23 +12,48 @@ import Dashboard from '../pages/Dashboard.jsx'
 import Transfer from '../pages/Transfer.jsx'
 import History from '../pages/History.jsx'
 import TopUpPage from '../pages/TopUpPage.jsx'
+import Profile from '../pages/Profile.jsx'
+import ChangePassword from '../pages/ChangePassword.jsx'
+import ChangePin from '../pages/ChangePin.jsx'
+import ForgotPassword from '../pages/ForgotPassword.jsx'
+import EnterPin from '../pages/EnterPin.jsx'
+import TransferDetail from '../pages/TransferDetail.jsx'
 
 // import Register from '../pages/Register.jsx'
 // import Dashboard from '../pages/Dashboard.jsx'
 
 function AppRoute() {
   return (
-    <Routes>
-      <Route path='/' element={<LandingPage />} />
-      <Route path='auth'>
-        <Route index element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Route>
-      <Route path='dashboard' element={<Dashboard />} />
-      <Route path='transfer' element={<Transfer />} />
-      <Route path='history' element={<History />} />
-      <Route path='topup' element={<TopUpPage/>} />
-    </Routes>
+    <>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Toaster position="top-center" reverseOrder={false} />
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='auth'>
+              <Route index element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="forgotpassword" element={<ForgotPassword />} />
+              <Route path="enterpin" element={<EnterPin />} />
+            </Route>
+            <Route element={<AuthProtect />}>
+              <Route path='dashboard' element={<Dashboard />} />
+              <Route path='transfer'>
+                <Route index element={<Transfer />} />
+                <Route path='detail' element={<TransferDetail />} />
+              </Route>
+              <Route path='history' element={<History />} />
+              <Route path='topup' element={<TopUpPage />} />
+              <Route path='profile'>
+                <Route index element={<Profile />} />
+                <Route path='changepassword' element={<ChangePassword />} />
+                <Route path='changepin' element={<ChangePin />} />
+              </Route>
+            </Route>
+          </Routes>
+        </PersistGate>
+      </Provider>
+    </>
   )
 }
 
