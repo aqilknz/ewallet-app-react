bb0a3360d7581b33f4e30e618c74c106e22b3f79
aqilknz <khairunnz123@gmail.com>
2026-04-22 23:43:26 +0700

    feat: add login and logout slice

--- src/redux/slice/authSlice.js
+++ src/redux/slice/authSlice.js
@@ -1,28 +1,36 @@
-import { createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
-    users: [],
-    currentUser: null,
-    isLogin: false
-}
+  currentUser: null,
+  isAuthenticated: false,
+  error: null,
+};
 
 const authSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        registerUser: (state, action) => {
-            state.users.push(action.payload)
-        },
-        loginSuccess: (state, action) => {
-            state.currentUser = action.payload;
-            state.isLogin = true
-        },
-        logout: (state) => {
-            // state.userAccount = null
-            state.currentUser = null;
-            state.isLogin= false
-        }
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.isAuthenticated = true;
+      state.error = null;
+    },
+    loginFailed: (state, action) => {
+      state.error = action.payload;
+      state.isAuthenticated = false;
+    },
+    logoutUser: (state) => {
+      state.currentUser = null;
+      state.isAuthenticated = false;
+      state.error = null;
+    },
+    updateCurrentUserInfo: (state, action) => {
+      if (state.currentUser) {
+        state.currentUser = { ...state.currentUser, ...action.payload };
+      }
     }
-})
+  },
+});
 
-export const {registerUser, loginSuccess, logout} = authSlice.actions
-export default authSlice.reducer
+export const { loginSuccess, loginFailed, logoutUser, updateCurrentUserInfo } = authSlice.actions;
+export default authSlice.reducer;
