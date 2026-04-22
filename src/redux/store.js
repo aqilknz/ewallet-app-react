997a24cede01ba51a28f95e9fb9d6ebea684cac7
aqilknz <khairunnz123@gmail.com>
2026-04-22 23:41:33 +0700

    feat: add regitser and transaction reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/es/storage"
-import userReducer from "./slice/authSlice"
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { 
+  persistStore, 
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -10,25 +9,40 @@ import {
   PURGE,
   REGISTER,
 } from "redux-persist";
+import storage from "redux-persist/es/storage"; // Import yang lebih standar
+import authReducer from "./slice/authSlice";
+import registerReducer from "./slice/registerSlice";
+import transactionReducer from "./slice/transactionSlice"
 
-const persistConfig ={
-    key: "usersAccount",
+// Konfigurasi untuk Sesi Login (Sangat penting agar user tidak perlu login ulang)
+export const authPersistConfig = {
+    key: "authSession",
     storage,
-}
-const persistedReducer = persistReducer(persistConfig,  userReducer)
+    whitelist: ['currentUser', 'isAuthenticated'] // Hanya simpan data login utama
+};
+
+// Konfigurasi untuk Database User (Poin a.i: List akun yang didaftarkan)
+export const registerPersistConfig = {
+    key: "registerSession",
+    storage,
+    whitelist: ['users'] // HANYA simpan daftar users, abaikan error/loading
+};
+
+const rootReducer = combineReducers({
+  auth: persistReducer(authPersistConfig, authReducer),
+  register: persistReducer(registerPersistConfig, registerReducer),
+  transaction: transactionReducer,
+});
 
 export const store = configureStore({
-    reducer: {
-        user: persistedReducer
-    },
-
+    reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
+        // Abaikan pengecekan serializable untuk action bawaan redux-persist
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
-})
+});
 
-export const persistor = persistStore(store)
-
+export const persistor = persistStore(store);
