4e3a571dbeb75dd746b0b40c4ea5b6cbf43771f6
aqilknz <khairunnz123@gmail.com>
2026-05-06 02:59:36 +0700

    feat: add layouting

--- src/pages/TransferDetail.jsx
+++ src/pages/TransferDetail.jsx
@@ -18,25 +18,17 @@ function TransferDetail() {
   }
 
   return (
-    <div className='flex flex-col min-h-screen bg-gray-50'>
-      <Header />
-      <div className='flex flex-1 overflow-hidden'>
-        <Sidebar />
-        <main className='flex-1 p-6 md:p-10 overflow-y-auto bg-white'>
-          <div className='max-w-6xl mx-auto'>
-            <div className='flex items-center gap-4 mb-8'>
-              <img src='/icons/Send-b.svg' alt="Transfer" className='w-6 h-6' />
-              <h2 className='font-bold text-xl text-gray-800'>Transfer Money</h2>
-            </div>
-            <div className="w-full py-4">
-              <TransferStep currentStep={2} />
-            </div>
-            <section>
-              <DetailTransfer user={selectedUser} />
-            </section>
-          </div>
-        </main>
+    <div className='relative'>
+      <div className='flex items-center gap-4 mb-8'>
+        <img src='/icons/Send-b.svg' alt="Transfer" className='w-6 h-6' />
+        <h2 className='font-bold text-xl text-gray-800'>Transfer Money</h2>
       </div>
+      <div className="w-full py-4">
+        <TransferStep currentStep={2} />
+      </div>
+      <section>
+        <DetailTransfer user={selectedUser} />
+      </section>
     </div>
   )
 }
