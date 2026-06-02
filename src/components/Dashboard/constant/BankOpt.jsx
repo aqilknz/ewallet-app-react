4ef9770cbfbcc0eb91f2223d7fa620ae07e8176f
aqilknz <khairunnz123@gmail.com>
2026-06-02 17:53:37 +0700

    feat: add bank options

---
+++ src/components/Dashboard/constant/BankOpt.jsx
@@ -0,0 +1,7 @@
+export const bankOpt = [
+    { id: 1, name: "Bank Rakyat Indonesia", icon: "/icons/bank/bri.svg" },
+    { id: 2, name: "Dana", icon: "/icons/bank/dana.svg" },
+    { id: 3, name: "Bank Central Asia", icon: "/icons/bank/bca.svg" },
+    { id: 4, name: "Gopay", icon: "/icons/bank/gopay.svg" },
+    { id: 5, name: "OVO", icon: "/icons/bank/ovo.svg" },
+];
