210fea9e56c6993bdcb7e0a2df4b37b31e4a2791
aqilknz <khairunnz123@gmail.com>
2026-06-03 02:14:27 +0700

    chore: vite api url

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:9000/ewallet";
 
 // API Autentikasi
 export const loginAPI = async (credentials) => {
